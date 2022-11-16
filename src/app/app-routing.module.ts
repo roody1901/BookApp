31f1c6090256e6b244d9ff029ae847d3e7ac724a
vibhanshu.pandey <vibhanshu.pandey@globallogic.com>
2022-11-16 21:24:02 +0530

    intial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthorComponent } from './author/author.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { RecomendedComponent } from './recomended/recomended.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:DashboardComponent},
+  {path:'favorite',component:FavoriteComponent},
+  {path:'recomended',component:RecomendedComponent},
+  {path:'author',component:AuthorComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/bookdata.service.ts
+++ src/app/bookdata.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookdataService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  books='https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyCeEYbG9HvMfUvTYx35R7Fh5vm-Kh3yRRY'
+  getBook():Observable<any>{
+    console.log(this.books);
+    return this.http.get(this.books);
+  }
 }
