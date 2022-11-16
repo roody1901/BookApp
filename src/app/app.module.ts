327854c16b3bdde63db47aa83dcfae6996ddc09a
vibhanshu.pandey <vibhanshu.pandey@globallogic.com>
2022-11-16 20:39:52 +0530

    Intial Comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FavoriteComponent } from './favorite/favorite.component';
+import { RecomendedComponent } from './recomended/recomended.component';
+import { AuthorComponent } from './author/author.component';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    FavoriteComponent,
+    RecomendedComponent,
+    AuthorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
