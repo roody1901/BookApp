454de55ba207298f3687eb63182f1450cb7ab081
vibhanshu.pandey <vibhanshu.pandey@globallogic.com>
2022-11-20 15:16:43 +0530

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,21 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthorComponent } from './author/author.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { SecurityGuard } from './guards/security.guard';
 import { LoginComponent } from './login/login.component';
 import { RecomendedComponent } from './recomended/recomended.component';
 import { RegisterComponent } from './register/register.component';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
-  {path:'dashboard',component:DashboardComponent},
-  {path:'favorite',component:FavoriteComponent},
-  {path:'recomended',component:RecomendedComponent},
-  {path:'author',component:AuthorComponent},
+  {path:'dashboard',component:DashboardComponent,canActivate:[SecurityGuard]},
+  {path:'favorite',component:FavoriteComponent,canActivate:[SecurityGuard]},
+  {path:'recomended',component:RecomendedComponent,canActivate:[SecurityGuard]},
+  {path:'author',component:AuthorComponent,canActivate:[SecurityGuard]},
   {path:'register',component:RegisterComponent},
   {path:'',component:LoginComponent},
   {path:'login',component:LoginComponent},
-  {path:'search',component:SearchComponent}
+  {path:'search',component:SearchComponent,canActivate:[SecurityGuard]}
 
 ];
 
---
+++ src/app/guards/security.guard.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Route, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SecurityGuard implements CanActivate {
+  constructor (private route:Router){}
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(localStorage.getItem('token')){
+        return true;
+      }
+      else{
+        this.route.navigateByUrl('/login');
+        return false;
+      }
+    
+  }
+  
+}
