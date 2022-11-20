dc2c483ee65b745f1e60b97a8500b768be0c97d2
vibhanshu.pandey <vibhanshu.pandey@globallogic.com>
2022-11-20 12:41:36 +0530

    intital commit

--- src/app/bookdata.service.ts
+++ src/app/bookdata.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {Observable} from 'rxjs';
 
+
 @Injectable({
   providedIn: 'root'
 })
@@ -9,17 +10,22 @@ export class BookdataService {
 
   constructor(private http:HttpClient) { }
 
-  // books='https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyCeEYbG9HvMfUvTYx35R7Fh5vm-Kh3yRRY'
+  
+  Recomendbooks='https://www.googleapis.com/books/v1/volumes?q=flowers+inauthor:keyes&key=AIzaSyCeEYbG9HvMfUvTYx35R7Fh5vm-Kh3yRRY'
+ 
   booksApi:any='https://example-data.draftbit.com/books?_limit=50'
   getBook():Observable<any>{
     console.log(this.booksApi);
     return this.http.get(this.booksApi);
   }
 
+  getRecomend():Observable<any>{
+    return this.http.get(this.Recomendbooks);
+  }
   addFav(favBook:any){
     return this.http.post('http://localhost:9000/auth/favorites',favBook,{
       headers:{
-        Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njg4NDc3MzIsImV4cCI6MTY2ODg2NTczMn0.809CSzB48eTHq9Zmj28UOYfDt2ds4xWTxTD49OjzXPI'
+        Authorization: `Bearer ${localStorage.getItem('token')}`
       }
     });
   }
@@ -27,7 +33,7 @@ export class BookdataService {
   getFav(){
     return this.http.get('http://localhost:9000/auth/favorites',{
       headers:{
-        Authorization:'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2Njg4NDc3MzIsImV4cCI6MTY2ODg2NTczMn0.809CSzB48eTHq9Zmj28UOYfDt2ds4xWTxTD49OjzXPI'
+        Authorization:`Bearer ${localStorage.getItem('token')}`
       }
     });
   }
--- src/app/recomended/recomended.component.ts
+++ src/app/recomended/recomended.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BookdataService } from '../bookdata.service';
 
 @Component({
   selector: 'app-recomended',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecomendedComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( private http:HttpClient,private bokdata:BookdataService) { }
+  booklist:any=[];
   ngOnInit(): void {
+    this.bokdata.getRecomend().subscribe((res:any)=>{
+      console.log(res.items);
+      this.booklist=res.items;
+    })
+
   }
 
 }
